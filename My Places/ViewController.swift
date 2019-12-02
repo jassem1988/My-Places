1a9081a18be05d68cbe573615d6386417da187cc
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-12-02 07:57:31 +0300

    Initial Commit

---
+++ My Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by Jassem Al-Buloushi on 12/2/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
