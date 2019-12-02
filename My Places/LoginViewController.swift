e84086b3b5182119045cd68750f200667b0f43a2
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-12-02 09:13:20 +0300

    - Create displayAlert func to show Alerts for the users - Create login and signup Authentications fro Firebase

--- My Places/LoginViewController.swift
+++ My Places/LoginViewController.swift
@@ -20,9 +20,25 @@ class LoginViewController: UIViewController {
     
     @IBOutlet var bottomButton: UIButton!
     
+    //Variables
+    var signUpMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - My Methods
+    
+    func displayAlert(title: String, message: String) {
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(ac, animated: true, completion: nil)
+        
     }
     
     
@@ -30,16 +46,89 @@ class LoginViewController: UIViewController {
     
     @IBAction func topButtonPressed(_ sender: UIButton) {
         
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            
+            displayAlert(title: "Missing Information", message: "You must provide both email and password ")
+            
+            
+        } else {
+            
+            if let email = emailTextField.text {
+                
+                if let password = passwordTextField.text {
+                    
+                    if signUpMode {
+                        
+                        //SIGNUP
+                        
+                        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                            
+                            if error != nil {
+                                
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                                
+                            } else {
+                                
+                                print("Signup Success!!")
+                                
+                            }
+                            
+                        }
+                        
+                        
+                    } else {
+                        
+                        //LOGIN
+                        
+                        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                            
+                            if error != nil {
+                                
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                                
+                            } else {
+                                
+                                print("Login Success!!")
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
         
     }
     
     
     @IBAction func bottomButtonPressed(_ sender: UIButton) {
         
+        if signUpMode {
+            
+            topButton.setTitle("Log In", for: .normal)
+            
+            bottomButton.setTitle("Switch to Signup", for: .normal)
+            
+            signUpMode = false
+            
+        } else {
+            
+            topButton.setTitle("Sign Up", for: .normal)
+            
+            bottomButton.setTitle("Switch to Login ", for: .normal)
+            
+            signUpMode = true
+            
+        }
+        
         
     }
     
-
-
+    
+    
 }
 
