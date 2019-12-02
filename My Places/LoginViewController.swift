a4eb1f648be38faf1a3c181ffc3abb8ae75c166c
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-12-02 08:40:26 +0300

    - Install Firebase pods and config - Create login page and connect it to loginVC

---
+++ My Places/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  My Places
+//
+//  Created by Jassem Al-Buloushi on 12/2/19.
+//  Copyright © 2019 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var topButton: UIButton!
+    
+    @IBOutlet var bottomButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    //MARK: - IB Actions
+    
+    @IBAction func topButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func bottomButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+
+
+}
+
